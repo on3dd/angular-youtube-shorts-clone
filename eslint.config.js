4c891225ca0a72ad34336202d9d083ac517c60ef
on3dd <onedeadwave.work@gmail.com>
2025-01-12 16:17:26 +1000

    repo: setup eslint & prettier

---
+++ eslint.config.js
@@ -0,0 +1,113 @@
+import nx from '@nx/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import angular from 'angular-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportsPlugin from 'eslint-plugin-simple-import-sort';
+import path from 'path';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default tseslint.config(
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: [
+      '**/dist',
+      '**/node_modules',
+      '**/database.types.ts',
+      'src/index.html',
+      '.angular',
+      '.nx',
+      'jest.{config,preset}.{js,ts}',
+    ],
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['tsconfig.json', 'tsconfig.*?.json'],
+      },
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+    // Override or add rules here
+    rules: {},
+  },
+  // General import rules
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      'simple-import-sort': simpleImportsPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-default-export': 'error',
+      'import/extensions': 'off',
+      'simple-import-sort/imports': 'error',
+      'sort-imports': 'off',
+      '@typescript-eslint/unbound-method': [
+        'error',
+        {
+          ignoreStatic: true,
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
+  {
+    files: ['**/*.ts'],
+    extends: [...angular.configs.tsRecommended],
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended],
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
+  {
+    files: ['eslint.config.js', 'src/main.server.ts'],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  },
+);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -14,9 +15,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-shorts-clone'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome angular-shorts-clone');
   });
 
   it(`should have as title 'angular-shorts-clone'`, () => {
