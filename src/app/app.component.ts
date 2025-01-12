c6003f38b8d46b9ccbcaca7d3701224661a2d2db
on3dd <onedeadwave.work@gmail.com>
2025-01-13 00:30:42 +1000

    feat: actions bar layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matThumbUp } from '@ng-icons/material-icons/baseline';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, NgIcon, NgClass],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideIcons({ matThumbUp })],
 })
 export class AppComponent {
   title = 'angular-shorts-clone';
