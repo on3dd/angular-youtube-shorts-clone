c600e50d62430bb09cbe4cf5294a21429b3f9f84
on3dd <onedeadwave.work@gmail.com>
2025-01-13 11:27:31 +1000

    feat: auto detect theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { matThumbUp } from '@ng-icons/material-icons/baseline';
+import { matComment, matMoreVert, matShare, matThumbDown, matThumbUp } from '@ng-icons/material-icons/baseline';
+
+type MenuAction = {
+  // name for screen readers
+  name: string;
+  icon: string;
+  label?: string;
+  active?: boolean;
+  // TODO: add on-click action
+};
 
 @Component({
   imports: [RouterModule, NgIcon, NgClass],
@@ -10,8 +19,16 @@ import { matThumbUp } from '@ng-icons/material-icons/baseline';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [provideIcons({ matThumbUp })],
+  providers: [provideIcons({ matThumbUp, matComment, matMoreVert, matShare, matThumbDown })],
 })
 export class AppComponent {
   title = 'angular-shorts-clone';
+
+  protected readonly actions: MenuAction[] = [
+    { name: 'Like', label: '356k', icon: 'matThumbUp', active: true },
+    { name: 'Dislike', label: 'Dislike', icon: 'matThumbDown' },
+    { name: 'Comment', label: '2 742', icon: 'matComment' },
+    { name: 'Share', label: 'Share', icon: 'matShare' },
+    { name: 'More', icon: 'matMoreVert' },
+  ];
 }
